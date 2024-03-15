ce6c827fbb176f1dbe3d73b8c3303d5dc861eb1e
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-03-15 22:30:51 +0300

    0.6 Сделал вывод Applications из базы данных на главную страницу

--- routes/groups/livewire/main_page.php
+++ routes/groups/livewire/main_page.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Main;
+use App\Livewire\MyTickets;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Main::class)->name('main');
+Route::get('/my-tickets', MyTickets::class)->name('my-tickets');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,2 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
 require __DIR__ . '/groups/livewire/main_page.php';
